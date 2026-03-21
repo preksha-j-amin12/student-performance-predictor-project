598e1d23ea8c8e88821ebbb0fa929aa028ce262f
preksha-j-amin12 <aminpreksha12@gmail.com>
2026-03-21 16:28:24 +0530

    Fix app run

--- app.py
+++ app.py
@@ -133,11 +133,8 @@ def predict():
         branch=branch)
 
     return render_template("predict.html")
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
-    import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Railway provides PORT dynamically
-    app.run(host="0.0.0.0", port=port, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
